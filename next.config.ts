99a956bbb3478ed9ab963ddded4afb70aeb6c723
Z User <z@container>
2026-04-28 20:50:00 +0000

    df868e35-2076-42c9-9753-966627d4b7f0

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
+  allowedDevOrigins: [
+    "preview-chat-495b6ada-0c70-4d3a-b6da-a49c1f6ee17c.space-z.ai",
+    "https://preview-chat-495b6ada-0c70-4d3a-b6da-a49c1f6ee17c.space-z.ai",
+    "http://preview-chat-495b6ada-0c70-4d3a-b6da-a49c1f6ee17c.space-z.ai",
+  ],
 };
 
 export default nextConfig;
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/z/my-project
+while true; do
+  bun run dev
+  sleep 2
+done
